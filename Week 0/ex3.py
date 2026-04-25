0f88d9d38ea780c5065560ccedd2e847ab398090
Muhammed-Awwal Balogun <awwalbalogun06@gmail.com>
2026-04-25 10:30:57 +0100

    week 0

---
+++ Week 0/ex3.py
@@ -0,0 +1,35 @@
+print("Addition")
+print(2 + 3) #5
+
+print("Subtraction")
+print(5 - 2) #3
+
+print("Multiplication")
+print(3 * 4) #12
+
+print("Division")
+print(10 / 2) #5.0
+
+print("Exponentiation")
+print(2 ** 3) #8
+
+print("Greater than")
+print(5 > 2) #True
+
+print("Less than")
+print(3 < 4) #True
+
+print("Greater than or equal to")
+print(5 >= 5) #True
+
+print("Less than or equal to")
+print(3 <= 4) #True
+
+print("Equal to")
+print(5 == 5) #True
+
+print("Not equal to")
+print(3 != 4) #True
+
+print("Floor division")
+print(10 // 3) #3
